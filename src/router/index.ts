6feee52bbaa1e0b7d1ffb1c49528b46c60fdbe2c
ShangYJQ <421207553@qq.com>
2025-07-31 04:20:21 +0800

    dev:change to hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,14 +4,14 @@
  * Automatic routes for `./src/pages/*.vue`
  */
 
-import {setupLayouts} from 'virtual:generated-layouts'
+import { setupLayouts } from 'virtual:generated-layouts'
 // Composables
-import {createRouter, createWebHistory} from 'vue-router/auto'
+import { createRouter, createWebHashHistory } from 'vue-router/auto'
 // eslint-disable-next-line import/no-duplicates
-import {routes} from 'vue-router/auto-routes'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: setupLayouts(routes),
 })
 
